fe6497794a34d8efa741eba03e2a8e637524129f
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-11-05 10:52:39 +1200

    fix referee for unwraping

--- verification/referee.py
+++ verification/referee.py
@@ -29,21 +29,9 @@ checkio.referee.cover_codes
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
-from checkio.referees import cover_codes
-from checkio.referees import checkers
 
 from tests import TESTS
 
 api.add_listener(
     ON_CONNECT,
-    CheckiOReferee(
-        tests=TESTS,
-        cover_code={
-            'python-27': cover_codes.unwrap_args,  # or None
-            'python-3': cover_codes.unwrap_args
-        },
-        # checker=None,  # checkers.float.comparison(2)
-        # add_allowed_modules=[],
-        # add_close_builtins=[],
-        # remove_allowed_modules=[]
-    ).on_ready)
+    CheckiOReferee(tests=TESTS).on_ready)
