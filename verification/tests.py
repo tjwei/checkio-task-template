2e407777bb239e0aeef15d931de04957f92eb4b9
Amachua <vidal@et.esiea.fr>
2013-11-04 11:05:11 +0100

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -1,13 +1,3 @@
-"""
-TESTS is a dict with all you tests.
-Keys for this will be categories' names.
-Each test is dict with
-    "input" -- input data for user function
-    "answer" -- your right answer
-    "explanation" -- not necessary key, it's using for additional info in animation.
-"""
-
-
 TESTS = {
     "Basics": [
         {
